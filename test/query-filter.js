ec5a6e6484116b69d076369053c35495c7550d12
Vincent Weevers <mail@vincentweevers.nl>
2015-05-13 14:04:10 +0200

    Fix operator precedence

--- test/query-filter.js
+++ test/query-filter.js
@@ -6,7 +6,7 @@ var test        = require('tape')
 test('query filter (does not use indexes)', function(t) {
   var db = createDb()
 
-  t.plan(15)
+  t.plan(19)
 
   db.batch([
     { key: 'readme',  value: {type: 'text', size: 30 }},
@@ -21,8 +21,14 @@ test('query filter (does not use indexes)', function(t) {
     q({size: {gt: 30}}, ['logo'], 'gt')
     q({size: {lt: 10}}, [], 'lt')
     q({size: {lte: 10}}, ['license'], 'lte')
-    q({size: {lte: 1000, gt: 10}}, ['logo', 'readme'], 'lte + gt')
+
     q({size: {eq: 10, gt: 10}}, ['license'], 'eq takes precedence')
+    q({size: {gt: 10, gte: 10}}, ['logo', 'readme'], 'gt takes precedence over gte')
+    q({size: {lt: 1000, lte: 1000}}, ['license', 'readme'], 'lt takes precedence over lte')
+
+    q({size: {lte: 1000, gt: 10}}, ['logo', 'readme'], 'lte + gt')
+    q({size: {lte: 1000, gt: 10, gte: 3000}}, ['logo', 'readme'], 'lte + gt + gte = lte + gt')
+    q({size: {lte: 1000, lt: 1000, gt: 10, gte: 3000}}, ['readme'], 'lte + lt + gte + gt = lt + gt')
 
     q({size: {gt: 0}}, ['license', 'logo', 'readme'], 'gt 0')
     q({size: {gt: null}}, ['license', 'logo', 'readme'], 'gt null')
